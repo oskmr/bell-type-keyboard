259b9852437563bbceafe5464b7f5aa4dcd0d691
oskmr <str3tech@gmail.com>
2026-01-29 03:22:24 +0900

    Initial Commit

---
+++ bell-type-keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bell-type-keyboard
+//
+//  Created by miseri.osaka on 2026/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bell-type-keyboard/bell_type_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  bell_type_keyboardApp.swift
+//  bell-type-keyboard
+//
+//  Created by miseri.osaka on 2026/01/29.
+//
+
+import SwiftUI
+
+@main
+struct bell_type_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
